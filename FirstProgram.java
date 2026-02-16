e53ac9fb4d71ed46bbe5cd87b26f3e969f27359c
Ahmad <AAhmad@thegoldensource.com>
2026-02-16 18:01:11 +0530

    hii

--- FirstProgram.java
+++ FirstProgram.java
@@ -2,6 +2,7 @@ public class FirstProgram {
 public static void main(String[] args) {
     System.out.println("this is working fine");
     System.out.println("something change in main method");
+    System.out.println("hello");
 }
     
 }
